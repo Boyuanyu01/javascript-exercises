900ce201b41a0435865a3874a479019880e0ce57
Boyuan <boyuanyu0715@outlook.com>
2022-06-20 15:24:48 -0500

    finished 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,15 @@
-const ftoc = function() {
-
+const ftoc = function(temp) {
+  if((temp - 32) % 9 === 0){
+    return (temp - 32) * 5 / 9;
+  }
+  return Math.round((temp - 32.0) * 5 / 9 * 10) / 10;
 };
 
-const ctof = function() {
-
+const ctof = function(temp) {
+  if(temp % 5 === 0){
+    return temp * 9 / 5 + 32;
+  }
+  return Math.round((temp * 9 / 5 + 32.0) * 10) / 10;
 };
 
 // Do not edit below this line
