f1d494378110ba659959c12018882bb5242cd6fa
Boyuan <boyuanyu0715@outlook.com>
2022-06-20 15:10:09 -0500

    finished 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,14 @@
 const sumAll = function() {
-
+    if(arguments.length != 2){
+        return 'ERROR';
+    }
+    if(typeof arguments[0] != 'number' || typeof arguments[1] != 'number'){
+        return 'ERROR';
+    }
+    if(arguments[0] < 0 || arguments[1] < 0){
+        return 'ERROR';
+    }
+    return (arguments[0] + arguments[1]) * (Math.abs(arguments[0] - arguments[1]) + 1) / 2;
 };
 
 // Do not edit below this line
