6bb3ff6e35ebe9e866505197d8dfe5520e9c7018
Boyuan <boyuanyu0715@outlook.com>
2022-06-20 14:59:08 -0500

    finished 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,9 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array) {
+	for(argument of arguments){
+		const found = array.find(element => element === argument);
+		array = array.filter(element => element != found);
+	}
+	return array;
 };
 
 // Do not edit below this line
