cbac6468a8ee71e7faa47bf20569d6fa078a9512
Boyuan <boyuanyu0715@outlook.com>
2022-06-21 23:41:03 -0500

    finished 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+	const replaced = string.replace(/[^a-z]/gi, '').toLowerCase();
+	let n = replaced.length;
+	for(let i = 0; i < n / 2; i++){
+		if(replaced.charAt(i) != replaced.charAt(n - i - 1)){
+			return false;
+		}
+	}
+	return true;
 };
 
 // Do not edit below this line
