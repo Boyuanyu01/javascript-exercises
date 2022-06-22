27e0963899d040dded6f66ff7f0327064a7a84c2
Boyuan <boyuanyu0715@outlook.com>
2022-06-22 00:09:58 -0500

    finished 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,24 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+	n = parseInt(n);
+	if(n <= 0){
+		return 'OOPS';
+	}
+	else{
+		if(n === 1){
+			return 1;
+		}
+		if(n === 2){
+			return 1;
+		}
+		let a = 1;
+		let b = 1;
+		for(let i = 3; i <= n; i++){
+			let temp = b;
+			b = a + b;
+			a = temp;
+		}
+		return b;
+	}
 };
 
 // Do not edit below this line
