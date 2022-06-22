57eb9dffdf910191fb8d71a4868f53d553fd7bd5
Boyuan <boyuanyu0715@outlook.com>
2022-06-22 01:12:13 -0500

    finished 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,28 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+	const ordered = people.sort(function(a, b) {
+		let a_age;
+		let b_age;
+		if(!('yearOfDeath' in a)){
+			a_age = new Date().getFullYear() - a.yearOfBirth;
+		}
+		else{
+			a_age = a.yearOfDeath - a.yearOfBirth;
+		}
+		if(!('yearOfDeath' in b)){
+                        b_age = new Date().getFullYear() - b.yearOfBirth;
+                }
+                else{
+                        b_age = b.yearOfDeath - b.yearOfBirth;
+                }
+		if(a_age > b_age){
+			return -1;
+		}
+		if(a_age < b_age){
+			return 1;
+		}
+		return 0;
+	})
+	return ordered[0];
 };
 
 // Do not edit below this line
