210e2f81d26a79bcc55df17e8eb1029425329516
Boyuan <boyuanyu0715@outlook.com>
2022-06-21 23:33:07 -0500

    finished 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,41 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(nums) {
+	let sums = 0;
+	for(let i = 0; i < nums.length; i++){
+		sums += nums[i];
+	}
+	return sums;
 };
 
-const multiply = function() {
-
+const multiply = function(nums) {
+	let prod = 1;
+	for(let i = 0; i < nums.length; i++){
+		prod *= nums[i];
+	}
+	return prod;
 };
 
-const power = function() {
-	
+const power = function(num, power) {
+	let prod = 1;
+	for(let i = 0; i < power; i++){
+		prod *= num;
+	}
+	return prod;
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+	let prod = 1;
+	for(let i = 1; i <= n; i++){
+		prod *= i;
+	}
+	return prod;
 };
 
 // Do not edit below this line
